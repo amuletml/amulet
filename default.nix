b8fdb996ad551ed873820474c9a2b36dbd45309d
Abigail Magalhães <magalhaes.alcantara@pucpr.edu.br>
2017-09-09 17:25:29 -0300

    Initial commit

---
+++ default.nix
@@ -0,0 +1,32 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, containers, mtl, parsec, stdenv
+      , transformers
+      }:
+      mkDerivation {
+        pname = "amuletml";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          base containers mtl parsec transformers
+        ];
+        homepage = "https://amulet.ml";
+        description = "A functional programming language";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
