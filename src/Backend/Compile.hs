04cc3b3a294d2be44e5b6c61c1963c30ca38bd20
SquidDev <bonzoweb@hotmail.co.uk>
2017-09-10 18:02:38 +0000

    Be smarter about emitting statements/expressions

--- src/Backend/Compile.hs
+++ src/Backend/Compile.hs
@@ -1,10 +1,16 @@
-module Backend.Compile where
+module Backend.Compile
+  ( compileProgram
+  , compileLet
+  , compileExpr
+  ) where
 
 import Syntax
 import Backend.Lua
 
+type Returner = Maybe (LuaExpr -> LuaStmt)
+
 compileProgram :: [Toplevel] -> LuaStmt
-compileProgram = LuaDo . compileProg where 
+compileProgram = LuaDo . compileProg where
   compileProg (ForeignVal n s _:xs) = LuaLocal [lowerName n] [LuaBitE s]:compileProg xs
   compileProg (ValStmt _ _:xs) = compileProg xs
   compileProg (LetStmt vs:xs) = locals ns vs' ++ compileProg xs where
@@ -16,19 +22,35 @@ compileLet (n, e) = (lowerName n, compileExpr e)
 
 compileExpr :: Expr -> LuaExpr
 compileExpr (VarRef v) = LuaRef (lowerName v)
-compileExpr (Let k s) = iife (locals ns vs ++ [LuaReturn (compileExpr s)]) where
-  (ns, vs) = unzip $ map compileLet k
-compileExpr (If c t e) = iife [LuaIf (compileExpr c) [LuaReturn (compileExpr t)]
-                                                     [LuaReturn (compileExpr e)]]
 compileExpr (App f x) = LuaCall (compileExpr f) [compileExpr x]
-compileExpr (Fun v e) = LuaFunction [lowerName v] [LuaReturn (compileExpr e)]
-compileExpr (Begin []) = iife []
-compileExpr (Begin xs) = iife (LuaLocal [LuaName "_"] []:(map (exprStmt . compileExpr) (init xs) ++ [LuaReturn (compileExpr (last xs))])) where
-  exprStmt = LuaAssign [LuaName "_"] . (:[])
+compileExpr (Fun (Capture v) e) = LuaFunction [lowerName v] (compileStmt (Just LuaReturn) e)
+compileExpr (Fun Wildcard e) = LuaFunction [LuaName "_"] (compileStmt (Just LuaReturn) e)
 compileExpr (Literal (LiInt x)) = LuaNumber (fromInteger x)
 compileExpr (Literal (LiStr str)) = LuaString str
 compileExpr (Literal (LiBool True)) = LuaTrue
 compileExpr (Literal (LiBool False)) = LuaFalse
+compileExpr s@(Let _ _) = compileIife s
+compileExpr s@(If _ _ _) = compileIife s
+compileExpr s@(Begin _) = compileIife s
+compileExpr s@(Match _ _) = compileIife s
+
+compileStmt :: Returner -> Expr -> [LuaStmt]
+compileStmt r e@(VarRef _) = pureReturn r $ compileExpr e
+compileStmt r e@(Literal _) = pureReturn r $ compileExpr e
+compileStmt r e@(Fun _ _) = pureReturn r $ compileExpr e
+compileStmt r (Let k c) = let (ns, vs) = unzip $ map compileLet k in
+                          (locals ns vs ++ compileStmt r c)
+compileStmt r (If c t e) = [LuaIf (compileExpr c) (compileStmt r t) (compileStmt r e)]
+compileStmt r (Begin xs) = concatMap (compileStmt Nothing) (init xs) ++ compileStmt r (last xs)
+
+compileStmt _ (Match _ []) = error "Cannot have empty match"
+compileStmt r (Match e ((Wildcard, c):_)) = compileStmt Nothing e ++ compileStmt r c
+compileStmt r (Match e ((Capture v, c):_)) = LuaLocal [lowerName v] [] :
+                                             compileStmt (Just (LuaAssign [lowerName v] . (:[]))) e ++
+                                             compileStmt r c
+
+compileStmt Nothing (App f x) = [LuaCallS (compileExpr f) [compileExpr x]]
+compileStmt (Just r) e@(App _ _) = [r (compileExpr e)]
 
 lowerName :: Var -> LuaVar
 lowerName (Refresh a k) = case lowerName a of
@@ -39,6 +61,10 @@ lowerName (Name a) = LuaName a
 iife :: [LuaStmt] -> LuaExpr
 iife b = LuaCall (LuaFunction [] b) []
 
+compileIife :: Expr -> LuaExpr
+compileIife = iife . compileStmt (Just LuaReturn)
+
+
 locals :: [LuaVar] -> [LuaExpr] -> [LuaStmt]
 locals xs ys = preDef ++ locals' xs ys where
   locals' (x:xs) (y:ys) = LuaAssign [x] [y]:locals xs ys
@@ -46,3 +72,7 @@ locals xs ys = preDef ++ locals' xs ys where
   preDef = case xs of
              [] -> []
              xs -> [LuaLocal xs []]
+
+pureReturn :: Returner -> LuaExpr -> [LuaStmt]
+pureReturn Nothing _ = []
+pureReturn (Just r) e = [r e]
