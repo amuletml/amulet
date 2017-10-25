ba3e3953d37b91a3dee2dae53ce8bc27260b560f
hydraz <urn@semi.works>
2017-10-25 17:17:11 -0200

    It's now broken in a different way

--- src/Types/Unify.hs
+++ src/Types/Unify.hs
@@ -42,9 +42,12 @@ unify t@(TyForall vs _ ty _) t'@(TyForall vs' _ ty' _)
   | length vs /= length vs' = throwError (NotEqual t t')
   -- TODO: Technically we should make fresh variables and do ty[vs/f] ~ ty'[vs'/f]
   | otherwise = unify ty (apply (M.fromList (zip vs' (map (flip TyVar internal) vs))) ty')
-unify (TyRows rho arow _) tb@(TyRows _ brow _)
+unify ta@(TyRows rho arow _) tb@(TyRows _ brow _)
   = let overlaps = overlap arow brow
      in do unify rho tb
+           if length overlaps == 0
+              then throwError (NoOverlap tb ta)
+              else pure ()
            forM_ overlaps $ \(a, b) -> unify a b
            pure ()
 unify x tp@(TyRows rho _ _) = throwError (Note (CanNotInstance rho tp x) isRec)
