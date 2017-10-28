3021ab647029e89099cf2efa0c88c95ba677faea
hydraz <urn@semi.works>
2017-10-28 15:29:28 -0200

    Better document the conditions for being well-formed
    And also format it as parameter + case so that getting the original type
    is easier.

--- src/Types/Wellformed.hs
+++ src/Types/Wellformed.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE FlexibleContexts #-}
-module Types.Wellformed where
+{-# LANGUAGE FlexibleContexts, LambdaCase #-}
+module Types.Wellformed (wellformed) where
 
 import Control.Monad.Except
 
@@ -9,20 +9,21 @@ import Syntax
 import Pretty(Pretty)
 
 wellformed :: (Pretty (Var p), MonadError TypeError m) => Type p -> m ()
-wellformed TyCon{} = pure ()
-wellformed TyVar{} = pure ()
-wellformed TyStar{} = pure ()
-wellformed (TyForall _ t _) = wellformed t
-wellformed (TyArr a b _) = wellformed a *> wellformed b
-wellformed (TyApp a b _) = wellformed a *> wellformed b
-wellformed tp@(TyRows rho rows _) = do
-  case rho of
-    TyRows{} -> pure ()
-    TyExactRows{} -> pure ()
-    TyVar{} -> pure ()
-    _ -> throwError (CanNotInstance tp rho)
-  mapM_ (wellformed . snd) rows
-wellformed (TyExactRows rows _) = mapM_ (wellformed . snd) rows
+wellformed tp = case tp of
+  TyCon{} -> pure ()
+  TyVar{} -> pure ()
+  TyStar{} -> pure ()
+  TyForall _ t _ -> wellformed t
+  TyArr a b _ -> wellformed a *> wellformed b
+  TyApp a b _ -> wellformed a *> wellformed b
+  TyRows rho rows _ -> do
+    case rho of
+      TyRows{} -> pure ()
+      TyExactRows{} -> pure ()
+      TyVar{} -> pure ()
+      _ -> throwError (CanNotInstance tp rho)
+    mapM_ (wellformed . snd) rows
+  TyExactRows rows _ -> mapM_ (wellformed . snd) rows
 
 {-
    Commentary:
@@ -32,15 +33,15 @@ wellformed (TyExactRows rows _) = mapM_ (wellformed . snd) rows
    Obviously, this module begs a bit of explaining. Since the kind
    inference engine in Types.Infer isn't usable in Type Typed, here we
    implement a very dumb wellformedness check that will reject
-   obviously-wrong types, such as { int | field : type}[1]. Hopefully in
-   the future we alleviate the need for this with a *proper* kind system
-   - so that we can reject instancing the hole in a polymorphic record
-   type to something that doesn't have the *kind* "list of rows".
+   obviously-wrong types, such as { int | field : type}. Hopefully in
+   the future we alleviate the need for this module with a *proper* kind
+   system
 
    Unfortunately, this is the best I can do right now.
 
 
-   [1] That's the only check we perform right now, because it's
-   basically the only thing we can check for, anyway - there aren't many
-   wellformed-ness conditions.
+   Checks we peform:
+    [1] Polymorphic record types' holes may only be instanced to
+    something "row-y", i.e. an exact record, another polymorphic record,
+    or a type variable. All other instancings are malformed.
      -}
