62f21fff4849beb0c747ca02a8791c509176f4c7
Abigail Magalhães <magalhaes.alcantara@pucpr.edu.br>
2017-12-17 09:23:11 -0200

    Strip “useless lets”
    Useless `let`s are those that bind a set of variable disjoint from the
    set of free variables in the expression directly below it. Instead of
    just erasing the variables, we raise them into a `begin` if needed.

--- src/Core/Simplify.hs
+++ src/Core/Simplify.hs
@@ -3,10 +3,12 @@ module Core.Simplify
   ) where
 
 import Core.Optimise.Match
-import Core.Optimise
 import Core.Optimise.Fold
+import Core.Optimise
 
 optimise :: [CoStmt] -> [CoStmt]
-optimise = runTransform . transformStmts (mconcat [ dropBranches
-                                                  , foldExpr
-                                                  ])
+optimise = runTransform . transformStmts
+  (mconcat [ dropBranches
+           , foldExpr
+           , dropUselessLets
+           ])
