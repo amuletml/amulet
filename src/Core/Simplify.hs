97e311b7f012aa3176f336aaae30fcd7965b0214
SquidDev <bonzoweb@hotmail.co.uk>
2018-01-05 14:30:03 +0000

    Continue optimising til a fix point
    Or for a maximum of 10 rounds. We should probably make this configurable
    at some point...

--- src/Core/Simplify.hs
+++ src/Core/Simplify.hs
@@ -1,5 +1,6 @@
 module Core.Simplify
   ( optimise
+  , optimiseOnce
   ) where
 
 import Core.Optimise.Match
@@ -8,7 +9,16 @@ import Core.Optimise.Eval
 import Core.Optimise
 
 optimise :: [CoStmt] -> [CoStmt]
-optimise = runTransform . transformStmts passes . peval where
+optimise = optimiseTil 10 where
+  optimiseTil :: Int -> [CoStmt] -> [CoStmt]
+  optimiseTil n stmts | n <= 0 = stmts
+                      | otherwise = let stmts' = optimiseOnce stmts
+                                    in if stmts == stmts'
+                                       then optimiseTil (n - 1) stmts'
+                                       else  stmts'
+
+optimiseOnce :: [CoStmt] -> [CoStmt]
+optimiseOnce = runTransform . transformStmts passes . peval where
   passes = mconcat [ dropBranches
                    , foldExpr
                    ]
