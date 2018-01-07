cad9fe21c77670fb27c18779cd22a41518c2311d
hydraz <urn@semi.works>
2018-01-07 11:25:14 -0200

    Implement match-of-known-constructor

--- src/Core/Optimise/Match.hs
+++ src/Core/Optimise/Match.hs
@@ -1,10 +1,15 @@
 module Core.Optimise.Match
-  ( dropBranches
+  ( dropBranches, matchKnownConstr
   ) where
 
 import qualified Data.Map.Strict as Map
+import Data.Triple
+import Data.Monoid
+import Data.Maybe
+import Data.List
 
 import Core.Optimise
+import Syntax (Var, Resolved)
 
 -- Attempts to simplify match expression, dropping redundant branches and
 -- replacing matches with flat expressions where possible.
@@ -22,3 +27,49 @@ dropBranches = beforePass' pass where
   reducePatterns [] = []
   reducePatterns (p@(CopCapture _ _, _, _):_) = [p]
   reducePatterns (p:xs) = p : reducePatterns xs
+
+matchKnownConstr :: TransformPass
+matchKnownConstr = afterPass go where
+  go :: CoTerm -> Trans CoTerm
+  go it@(CotMatch e ptrns) = do
+    weCan <- canWe e
+    pure $ if weCan
+              then fromMaybe it (doIt e ptrns)
+              else it
+  go x = pure x
+
+  canWe :: CoTerm -> Trans Bool
+  canWe (CotRef v _) = isCon v
+  canWe (CotApp f _) = canWe f
+  canWe (CotTyApp x _) = canWe x
+  canWe (CotExtend t xs) = do
+    t' <- canWe t
+    xs' <- mapM (canWe . thd3) xs
+    pure (t' && and xs')
+  canWe _ = pure False
+
+  doIt :: CoTerm -> [(CoPattern, CoType, CoTerm)] -> Maybe CoTerm
+  doIt x ((p, _, k):ps)
+    | Just binds <- match p (stripTyApp x) = Just (CotLet binds k)
+    | otherwise = doIt x ps
+  doIt _ _ = Nothing
+
+  match :: CoPattern -> CoTerm -> Maybe [(Var Resolved, CoType, CoTerm)]
+  match (CopCapture v t) x = Just [(v, t, x)]
+  match (CopConstr v) (CotRef v' _)
+    | v == v' = Just []
+    | otherwise = Nothing
+  match (CopDestr v p) (CotApp (CotRef v' _) x)
+    | v == v' = match p x
+    | otherwise = Nothing
+  match (CopExtend p xs) (CotExtend e ys) = match p e <> rows where
+    xs' = sortOn fst xs
+    ys' = sortOn fst (map (\(x, _, y) -> (x, y)) ys)
+    rows = fmap concat $ sequence (zipWith mr xs' ys')
+    mr (t, p) (t', e)
+      | t == t' = match p e
+      | otherwise = Nothing
+  match (CopLit l) (CotLit l')
+    | l == l' = Just []
+    | otherwise = Nothing
+  match _ _ = Nothing
