1e9eab433b95bb0e929a6fbe01b917781bbc8de0
hydraz <urn@semi.works>
2018-01-10 17:39:42 -0200

    Depend on Alex & Happy

--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc822" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc822", doBenchmark ? false }:
 
 let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, base, syb, containers, monad-gen, mtl
-      , parsec, stdenv, text, transformers, pretty-show
+  f = { mkDerivation, alex, base, containers, happy, monad-gen, mtl
+      , parsec, pretty-show, stdenv, syb, text, transformers
       }:
       mkDerivation {
         pname = "amuletml";
@@ -14,14 +14,22 @@ let
         isLibrary = false;
         isExecutable = true;
         executableHaskellDepends = [
-          base syb containers monad-gen mtl parsec text transformers
-          pretty-show # This is here for ghci prettiness
+          base containers monad-gen mtl parsec pretty-show syb text
+          transformers alex happy
         ];
         homepage = "https://amulet.ml";
         description = "A functional programming language";
         license = stdenv.lib.licenses.bsd3;
       };
 
-  haskellPackages = pkgs.haskell.packages.${compiler};
-  drv = haskellPackages.callPackage f {};
-in if pkgs.lib.inNixShell then drv.env else drv
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
