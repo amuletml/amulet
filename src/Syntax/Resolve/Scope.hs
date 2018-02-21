8061c55cb841ee59a177f6f89b8fd700fdaae6c7
SquidDev <bonzoweb@hotmail.co.uk>
2018-02-21 20:10:37 +0000

    Detect ambiguous references in the resolver

--- src/Syntax/Resolve/Scope.hs
+++ src/Syntax/Resolve/Scope.hs
@@ -3,6 +3,7 @@
 
 module Syntax.Resolve.Scope
   ( Scope(..)
+  , ScopeVariable(..)
   , builtinScope, tagVar
   , extend, extendN
   , extendTy, extendTyN
@@ -15,8 +16,13 @@ import Control.Monad.Reader
 
 import Syntax
 
-data Scope = Scope { varScope :: Map.Map (Var Parsed) (Var Resolved)
-                   , tyScope  :: Map.Map (Var Parsed) (Var Resolved)
+data ScopeVariable
+  = SVar (Var Resolved)
+  | SAmbiguous [Var Resolved]
+  deriving (Eq, Ord, Show)
+
+data Scope = Scope { varScope :: Map.Map (Var Parsed) ScopeVariable
+                   , tyScope  :: Map.Map (Var Parsed) ScopeVariable
                    }
   deriving (Eq, Ord, Show)
 
@@ -27,25 +33,31 @@ builtinScope = Scope
                                   , "||", "&&" ]
                , tyScope = build [ "int", "string", "bool", "unit" ]
                }
-  where build :: [T.Text] -> Map.Map (Var Parsed) (Var Resolved)
-        build = foldr (\v -> Map.insert (Name v) (TgInternal v)) mempty
+  where build :: [T.Text] -> Map.Map (Var Parsed) ScopeVariable
+        build = foldr (\v -> Map.insert (Name v) (SVar (TgInternal v))) mempty
 
 
 tagVar :: MonadGen Int m => Var Parsed -> m (Var Resolved)
 tagVar (Name n) = TgName n <$> gen
 
+insert :: Var Parsed -> Var Resolved -> Map.Map (Var Parsed) ScopeVariable -> Map.Map (Var Parsed) ScopeVariable
+insert v v' scope = case Map.lookup v scope of
+                      Nothing -> Map.insert v (SVar v') scope
+                      Just (SVar x) -> Map.insert v (SAmbiguous [v', x]) scope
+                      Just (SAmbiguous xs) -> Map.insert v (SAmbiguous (v':xs)) scope
+
 extend :: (MonadGen Int m, MonadReader Scope m) => (Var Parsed, Var Resolved) -> m a -> m a
 extend (v, v') =
-  local (\x -> x { varScope = Map.insert v v' (varScope x) })
+  local (\x -> x { varScope = insert v v' (varScope x) })
 
 extendN :: (MonadGen Int m, MonadReader Scope m) => [(Var Parsed, Var Resolved)] -> m a -> m a
 extendN vs =
-  local (\x -> x { varScope = foldr (uncurry Map.insert) (varScope x) vs })
+  local (\x -> x { varScope = foldr (uncurry insert) (varScope x) vs })
 
 extendTy :: (MonadGen Int m, MonadReader Scope m) => (Var Parsed, Var Resolved) -> m a -> m a
 extendTy (v, v') =
-  local (\x -> x { tyScope = Map.insert v v' (tyScope x) })
+  local (\x -> x { tyScope = insert v v' (tyScope x) })
 
 extendTyN :: (MonadGen Int m, MonadReader Scope m) => [(Var Parsed, Var Resolved)] -> m a -> m a
 extendTyN vs =
-  local (\x -> x { tyScope = foldr (uncurry Map.insert) (tyScope x) vs })
+  local (\x -> x { tyScope = foldr (uncurry insert) (tyScope x) vs })
