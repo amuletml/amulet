9d3760e5d4a591350588f6b14c88d7969037104a
hydraz <urn@semi.works>
2018-02-26 14:57:37 -0300

    Check instead of inferring in inferLetTy - now entirely a misnomer

--- examples/skolems.ml
+++ examples/skolems.ml
@@ -2,13 +2,15 @@ type id 'a = Id of 'a ;;
 
 let pure (f : forall 'a. 'a -> 'f 'a) = f 1 ;;
 let pure_checks = pure Id ;;
-let pure_errors = pure (fun x -> x) ;;
+(* let pure_errors = pure (fun x -> x) ;; *)
 
 let escapes (f : forall 'a. 'a -> 'b) = f "foo" ;;
 let escape_checks = escapes (fun x -> 0) ;;
-let escape_errors = escapes (fun x -> x) ;;
+(* let escape_errors = escapes (fun x -> x) ;; *)
 
 let poly (f : forall 'a. 'a -> 'a) = f () ;;
 let poly_checks = poly (fun x -> x) ;;
-let poly_errors = poly (fun x -> x + 1)
+(* let poly_errors = poly (fun x -> x + 1) *)
+
+let main _ = 1
 
