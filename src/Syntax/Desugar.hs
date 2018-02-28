18033fcd81f5bd0686b0c416049b09c4c4ff5e0b
Abigail Magalhães <magalhaes.alcantara@pucpr.edu.br>
2018-02-28 20:00:53 -0300

    Fixes to desugaring and tagging operators.
    Desugaring is hell, and so is inference. Save me.

--- src/Syntax/Desugar.hs
+++ src/Syntax/Desugar.hs
@@ -13,19 +13,29 @@ import Generics.SYB
 desugarProgram :: forall m. MonadGen Int m => [Toplevel Parsed] -> m [Toplevel Parsed]
 desugarProgram = everywhereM (mkM defaults) where
   defaults :: Expr Parsed -> m (Expr Parsed)
+  -- (+)
   defaults (BothSection op an) = do
     (ap, ar) <- fresh an
     (bp, br) <- fresh an
     pure (Fun ap (Fun bp (BinOp ar op br an) an) an)
+
+  -- (+ foo)
   defaults (LeftSection op vl an) = do
-    (cap, ref) <- fresh an
-    pure (Fun cap (BinOp ref op vl an) an)
-  defaults (RightSection op vl an) = do
-    (cap, ref) <- fresh an
-    pure (Fun cap (BinOp vl op ref an) an)
+    (cap, rhs) <- fresh an
+    let go lhs = pure (Fun cap (BinOp rhs op lhs an) an)
+    case vl of
+      VarRef{} -> go vl
+      Literal{} -> go vl
+      _ -> do
+        (Capture lv _, ref) <- fresh an
+        Let [(lv, vl, an)] <$> go ref <*> pure an
+
+  -- (foo +)
+  defaults (RightSection vl op an) = pure (App op vl an)
   defaults (AccessSection key an) = do
     (cap, ref) <- fresh an
     pure (Fun cap (Access ref key an) an)
+
   defaults x = pure x
 
 fresh :: MonadGen Int m => Ann Parsed -> m (Pattern Parsed, Expr Parsed)
