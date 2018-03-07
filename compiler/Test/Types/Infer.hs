a5e8fd79f873a81f66bf6154a464c49691449147
Abigail Magalhães <magalhaes.alcantara@pucpr.edu.br>
2018-03-07 14:14:24 -0300

    Test the compiler with Hedgehog (#20)

---
+++ compiler/Test/Types/Infer.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE PackageImports, TemplateHaskell, OverloadedStrings, FlexibleContexts #-}
+module Test.Types.Infer where
+
+import Control.Monad
+
+import Hedgehog
+
+import Test.Types.Util
+import Test.Syntax.Gen
+
+import Pretty (pretty)
+
+prop_wellTyped :: Property
+prop_wellTyped = property $ do
+  ty <- forAll genType
+  ex <- forAll (genCorrectExpr ty)
+  case inferExpr ex of
+    Left e -> footnote (show (pretty e)) *> failure
+    Right ty' -> guard (ty `equivalent` ty')
+
+prop_generatedChecks :: Property
+prop_generatedChecks = property $ do
+  ty <- forAll genType
+  ex <- forAll (genCorrectExpr ty)
+  case checkExpr ex ty of
+    Left e -> footnote (show (pretty e)) *> failure
+    Right () -> pure ()
+
+prop_findsErrors :: Property
+prop_findsErrors = property $ do
+  ex <- forAll genBadExpr
+  case inferExpr ex of
+    Left _ -> success
+    Right t -> footnote ("Found type " ++ show (pretty t) ++ " for " ++ show (pretty ex)) *> failure
+
+tests :: IO Bool
+tests = checkParallel $$(discover)
