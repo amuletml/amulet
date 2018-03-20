c580ddf0124118209c46f11592450bfe536748f0
hydraz <urn@semi.works>
2018-03-20 15:29:41 -0300

    ughr

--- src/Syntax/Raise.hs
+++ src/Syntax/Raise.hs
@@ -69,4 +69,3 @@ raiseCo :: (Var p -> Var p') -> Coercion p -> Coercion p'
 raiseCo v (VarCo a) = VarCo (v a)
 raiseCo v (ReflCo t t') = ReflCo (raiseT v t) (raiseT v t')
 raiseCo v (CompCo c c') = CompCo (raiseCo v c) (raiseCo v c')
-raiseCo v _ = undefined
