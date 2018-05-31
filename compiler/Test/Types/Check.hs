c53c3dc72105278b9956d27d06e83277ebacd72e
Abigail Magalhães <magalhaes.alcantara@pucpr.edu.br>
2018-05-31 14:42:22 -0300

    Feature: Implicit Laziness (#63)

--- compiler/Test/Types/Check.hs
+++ compiler/Test/Types/Check.hs
@@ -52,4 +52,5 @@ tests = do
   gadts <- testGroup "GADT inference tests" <$> goldenDir result "tests/gadt/" ".ml"
   rankn <- testGroup "Rank-N inference tests" <$> goldenDir result "tests/rankn/" ".ml"
   visinst <- testGroup "Visible Instantiation inference tests" <$> goldenDir result "tests/visinst/" ".ml"
-  pure (testGroup "Type inference" [ inference, gadts, rankn, visinst ])
+  lazy <- testGroup "Automatic laziness tests" <$> goldenDir result "tests/lazy/" ".ml"
+  pure (testGroup "Type inference" [ inference, gadts, rankn, visinst, lazy ])
