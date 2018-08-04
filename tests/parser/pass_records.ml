5c68351e23853918e6bf64731e56ecb0ebcbc0c8
SquidDev <bonzoweb@hotmail.co.uk>
2018-08-04 13:25:34 +0000

    Add "shorthand" record literals and patterns
    This allows writing { x, y } instead of { x = x, y = y }. While this is
    probably most useful in patterns, it's nice to have in expressions too.

--- tests/parser/pass_records.ml
+++ tests/parser/pass_records.ml
@@ -1,10 +1,24 @@
-let a = { x = 1, y = 2, z = 3 }
+let x = 1
+and y = 2
+and z = 3
 
-let b = { a with y = 3 }
+(* Expressions *)
+let basic_record = { x = 1, y = 2, z = 3 }
 
-let c = match b with
-        | { x = 1, y = y } -> y
+let short_record = { x, y, z }
 
-let d : {  x: int, y : int, z : int } = a
+let basic_extension = { basic_record with y = 3 }
 
-let e : { 'a | x: int, y : int } = a
+let short_extension = { short_record with y }
+
+(* Patterns *)
+let basic_pattern = function
+  | { x = 1, y = y } -> y
+
+let short_pattern = function
+  | { x, y } -> y
+
+(* Types *)
+let record_type : {  x: int, y : int, z : int } = a
+
+let record_type_extension : { 'a | x: int, y : int } = a
