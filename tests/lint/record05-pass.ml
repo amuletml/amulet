d3c1fd3f8ae97a74a7e9e609754012a26957cd56
Abigail Magalhães <magalhaes.alcantara@pucpr.edu.br>
2018-08-08 22:11:27 -0300

    Do pointwise subsumption for Rows<=Rows problems

---
+++ tests/lint/record05-pass.ml
@@ -0,0 +1,8 @@
+let apply
+  : forall 'row 'a 'b.
+    { 'row | apply : forall 'a 'b. ('a -> 'b) -> 'a -> 'b }
+    -> ('a -> 'b) -> 'a -> 'b
+  = fun r -> r.apply
+
+let main =
+  apply ({ apply = fun x -> x }) (fun x -> x + 1) 123
