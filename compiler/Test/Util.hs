ac22e586c0ef379277f3ff437e2f093b1b0e5b49
Jonathan Coates <git@squiddev.cc>
2018-08-15 22:22:32 +0000

    Allow golden tests to be regenerated
     - Split golden tests into a separate module
     - Add --regenerate|-r flag

--- compiler/Test/Util.hs
+++ compiler/Test/Util.hs
@@ -1,25 +1,22 @@
-{-# LANGUAGE OverloadedStrings, MultiParamTypeClasses, FlexibleContexts #-}
-module Test.Util where
+{-# LANGUAGE MultiParamTypeClasses, FlexibleContexts #-}
+module Test.Util
+  ( displayPlain
+  , displayPlainVerbose
+  , hedgehog
+  , module Test.Golden
+  ) where
 
 import Hedgehog
 import Hedgehog.Internal.Property (unGroupName, unPropertyName)
 
 import Test.Tasty.Hedgehog
-import Test.Tasty.HUnit
+import Test.Golden
 import Test.Tasty
 
-import qualified Data.Text.IO as T
 import qualified Data.Text as T
-import Data.Algorithm.Diff
-import Data.Maybe
-import Data.List
 
 import qualified Text.Pretty.Note as N
 import Text.Pretty.Semantic
-
-import Control.Exception
-
-import System.Directory
 
 displayPlain, displayPlainVerbose :: N.NoteDoc Style -> T.Text
 displayPlain
@@ -37,43 +34,3 @@ displayPlainVerbose
 hedgehog :: Group -> TestTree
 hedgehog Group { groupName = n, groupProperties = ps }
   = testGroup (unGroupName n) (map (\(n, p) -> testProperty (unPropertyName n) p) ps)
-
-golden :: FilePath -> T.Text -> Assertion
-golden expFile result = do
-  expected <- (Just <$> T.readFile expFile) `catch` catchIO
-  case expected of
-    Nothing -> do
-      T.writeFile expFile result
-      assertFailure "File does not exist, generated"
-    Just expected -> do
-      let expected' = process expected
-      let result' = process result
-      if expected' /= result' then
-        let contextDiff = getDiff expected' result'
-        in assertFailure ("\27[0m" ++ T.unpack (formatDoc contextDiff))
-      else pure ()
-
-  where
-    catchIO :: IOException -> IO (Maybe a)
-    catchIO _ = pure Nothing
-
-    process = T.split (=='\n') . T.dropAround (=='\n')
-
-    formatDoc :: [Diff T.Text] -> T.Text
-    formatDoc [] = ""
-    formatDoc (Both l _:d)  =          " " <> l <>       "\n" <> formatDoc d
-    formatDoc (First l:d)   = "\27[1;31m-" <> l <> "\27[0m\n" <> formatDoc d
-    formatDoc (Second l:d)  = "\27[1;32m+" <> l <> "\27[0m\n" <> formatDoc d
-
-goldenFile :: (FilePath -> T.Text -> T.Text) -> FilePath -> String -> String -> TestTree
-goldenFile fn dir name out = testCase name $ do
-  actual <- fn name <$> T.readFile (dir ++ name)
-  golden (dir ++ out) actual
-
-goldenDirOn :: (FilePath -> T.Text -> T.Text) -> (String -> String) -> FilePath -> String -> IO [TestTree]
-goldenDirOn fn out dir ext = mapMaybe (\x -> goldenFile fn dir x . out <$> spanTail ext x) . sort <$> listDirectory dir where
-  spanTail _ [] = Nothing
-  spanTail s x@(y:ys) = if x == s then Just [] else (y:) <$> spanTail s ys
-
-goldenDir :: (FilePath -> T.Text -> T.Text) -> FilePath -> String -> IO [TestTree]
-goldenDir = flip goldenDirOn (++".out")
