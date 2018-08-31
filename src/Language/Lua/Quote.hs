e97fb62d529053394471c1f4e3ae3cbff1489b08
SquidDev <bonzoweb@hotmail.co.uk>
2018-08-31 07:27:05 +0000

    Allow specifying Lua patterns too
    Well, this was somewhat grim

--- src/Language/Lua/Quote.hs
+++ src/Language/Lua/Quote.hs
@@ -26,35 +26,48 @@ luaStmts = luaQuote parseStmts
 
 luaQuote :: Data a => Parser a -> QuasiQuoter
 luaQuote parser = QuasiQuoter
-  { quoteExp = go
-  , quotePat = const (fail "Patterns not supported")
+  { quoteExp = go (dataToExpQ (antis TH.varE liftTE))
+  , quotePat = go (dataToPatQ (antis TH.varP liftTP))
   , quoteType = const (fail "Types not supported")
   , quoteDec = const (fail "Declarations not supported")
   } where
-  go s = do
+
+  go build s = do
     loc <- TH.location
     let (line, col)  = TH.loc_start loc
         pos = SourcePos (TH.loc_filename loc) line col
         spans = [( TH.loc_filename loc
                  , T.replicate (line - 1) "\n" <> T.replicate (col - 1) " " <> T.pack s)]
     case runParser pos (L.pack s) parser of
-      Right res -> antiLua res
+      Right res -> build res
       Left e -> fail . show . N.format (N.fileSpans spans) $ e
 
-antiLua ::  Data a => a -> TH.Q TH.Exp
-antiLua = dataToExpQ (const Nothing `extQ` antiExpr `extQ` antiStmt `extQ` antiVar `extQ` liftText)
+  antis var other =
+    const Nothing
+    `extQ` antiExpr var
+    `extQ` antiStmt var
+    `extQ` antiVar var
+    `extQ` other
 
-antiExpr :: LuaExpr -> Maybe (TH.Q TH.Exp)
-antiExpr (LuaQuoteE x) = Just $ TH.varE  (TH.mkName (T.unpack x))
-antiExpr _ = Nothing
+  liftTE :: T.Text -> Maybe (TH.Q TH.Exp)
+  liftTE = Just . liftText
 
-antiVar :: LuaVar -> Maybe (TH.Q TH.Exp)
-antiVar (LuaQuoteV x) = Just $ TH.varE  (TH.mkName (T.unpack x))
-antiVar _ = Nothing
+  liftTP :: T.Text -> Maybe (TH.Q TH.Pat)
+  liftTP txt = Just $ do
+    txt' <- liftText txt
+    pure (TH.ViewP (TH.AppE (TH.VarE '(==)) txt') (TH.ConP 'True []))
 
-antiStmt :: LuaStmt -> Maybe (TH.Q TH.Exp)
-antiStmt (LuaQuoteS x) = Just $ TH.varE  (TH.mkName (T.unpack x))
-antiStmt _ = Nothing
+antiExpr :: (TH.Name -> TH.Q a) -> LuaExpr -> Maybe (TH.Q a)
+antiExpr var (LuaQuoteE x) = Just $ var . TH.mkName . T.unpack $ x
+antiExpr _ _ = Nothing
 
-liftText :: T.Text -> Maybe (TH.Q TH.Exp)
-liftText txt = Just $ TH.AppE (TH.VarE 'T.pack) <$> TH.lift (T.unpack txt)
+antiVar :: (TH.Name -> TH.Q a) -> LuaVar -> Maybe (TH.Q a)
+antiVar var (LuaQuoteV x) = Just $ var . TH.mkName . T.unpack $ x
+antiVar _ _ = Nothing
+
+antiStmt :: (TH.Name -> TH.Q a) -> LuaStmt -> Maybe (TH.Q a)
+antiStmt var (LuaQuoteS x) = Just . var . TH.mkName . T.unpack $ x
+antiStmt _ _ = Nothing
+
+liftText :: T.Text -> TH.Q TH.Exp
+liftText txt = TH.AppE (TH.VarE 'T.pack) <$> TH.lift (T.unpack txt)
