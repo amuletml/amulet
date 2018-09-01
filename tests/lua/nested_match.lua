62a659a468f91807dbe3abba0e4dfbdcd28bcbf1
SquidDev <bonzoweb@hotmail.co.uk>
2018-09-01 07:55:17 +0000

    Add a primitive unboxing or uncurrying pass
    This will attempt to convert curried functions into uncurried ones,
    which take unboxed tuples instead.

--- tests/lua/nested_match.lua
+++ tests/lua/nested_match.lua
@@ -2,36 +2,33 @@ do
   local __builtin_unit = { __tag = "__builtin_unit" }
   local Nil = { __tag = "Nil" }
   local function Cons(x) return { __tag = "Cons", x } end
-  local zip
-  zip = function(f)
-    return function(xs)
-      return function(ys)
-        if xs.__tag == "Nil" then
-          return Cons({ _1 = 1, _2 = Nil })
-        elseif xs.__tag == "Cons" then
-          local hh = xs[1]
-          if ys.__tag == "Nil" then
-            return Cons({ _1 = 2, _2 = Nil })
-          elseif ys.__tag == "Cons" then
-            local hi = ys[1]
-            local hk = hi._1
-            local hj = hi._2
-            local hn = hh._1
-            local hm = hh._2
-            if 0 == hn then
-              if 0 == hk then
-                return Cons({ _1 = 3, _2 = Nil })
-              else
-                return Cons({ _1 = f(0)(hk), _2 = zip(f)(hm)(hj) })
-              end
-            else
-              return Cons({ _1 = f(hn)(hk), _2 = zip(f)(hm)(hj) })
-            end
+  local zip, zip0
+  zip = function(jx_1, jx_2, jx_3)
+    if jx_2.__tag == "Nil" then
+      return Cons({ _1 = 1, _2 = Nil })
+    elseif jx_2.__tag == "Cons" then
+      local hh = jx_2[1]
+      if jx_3.__tag == "Nil" then
+        return Cons({ _1 = 2, _2 = Nil })
+      elseif jx_3.__tag == "Cons" then
+        local hi = jx_3[1]
+        local hk = hi._1
+        local hj = hi._2
+        local hn = hh._1
+        local hm = hh._2
+        if 0 == hn then
+          if 0 == hk then
+            return Cons({ _1 = 3, _2 = Nil })
+          else
+            return Cons({ _1 = jx_1(0)(hk), _2 = zip(jx_1, hm, hj) })
           end
+        else
+          return Cons({ _1 = jx_1(hn)(hk), _2 = zip(jx_1, hm, hj) })
         end
       end
     end
   end
+  zip0 = function(f) return function(xs) return function(ys) return zip(f, xs, ys) end end end
   local bottom = nil
-  bottom(zip)
+  bottom(zip0)
 end
