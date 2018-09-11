93dcfe45dce730fb9f416563a98c1237e99c20e7
hydraz <urn@semi.works>
2018-09-11 23:15:22 +0800

    Use Hashable CoVar instead of ints

--- src/Data/VarMap.hs
+++ src/Data/VarMap.hs
@@ -11,17 +11,13 @@ module Data.VarMap
   ) where
 
 import qualified Data.HashMap.Strict as Map
-import qualified Data.Text as T
-import qualified Data.List as L
 import Data.Coerce
 import Prelude hiding (lookup, map, null)
-
-import Control.Arrow
 
 import Core.Var
 
 newtype Map a
-  = Map (Map.HashMap Int a)
+  = Map (Map.HashMap CoVar a)
   deriving (Eq, Show, Ord)
   deriving newtype (Semigroup, Monoid, Functor, Foldable)
 
@@ -29,25 +25,25 @@ null :: Map a -> Bool
 null (Map m) = Map.null m
 
 insert :: CoVar -> a -> Map a -> Map a
-insert (CoVar x _ _) v (Map k) = Map (Map.insert x v k)
+insert x v (Map k) = Map (Map.insert x v k)
 
 delete :: CoVar -> Map a -> Map a
-delete (CoVar x _ _) (Map k) = Map (Map.delete x k)
+delete x (Map k) = Map (Map.delete x k)
 
 fromList :: [(CoVar, a)] -> Map a
 fromList = foldr (uncurry insert) mempty
 
 toList :: Map a -> [(CoVar, a)]
-toList (Map m) = L.map (first create) (Map.toList m)
+toList (Map m) = Map.toList m
 
 member :: CoVar -> Map a -> Bool
-member (CoVar x _ _) (Map m) = Map.member x m
+member x (Map m) = Map.member x m
 
 findWithDefault :: a -> CoVar -> Map a -> a
-findWithDefault d (CoVar x _ _) (Map m) = Map.lookupDefault d x m
+findWithDefault d x (Map m) = Map.lookupDefault d x m
 
 lookup :: CoVar -> Map a -> Maybe a
-lookup (CoVar x _ _) (Map m) = Map.lookup x m
+lookup x (Map m) = Map.lookup x m
 
 union :: Map a -> Map a -> Map a
 union (Map a) (Map b) = Map (Map.union a b)
@@ -62,19 +58,13 @@ map :: (a -> b) -> Map a -> Map b
 map f (Map a) = Map (Map.map f a)
 
 mapWithKey :: (CoVar -> a -> b) -> Map a -> Map b
-mapWithKey f (Map a) = Map (Map.mapWithKey (f . create) a)
-
--- filter :: (CoVar -> Bool) -> Map a -> Map a
--- filter f (Map m) = Map (Map.filter (f . create) m)
+mapWithKey f (Map a) = Map (Map.mapWithKey f a)
 
 singleton :: CoVar -> a ->  Map a
-singleton (CoVar x _ _) v = coerce (Map.singleton x v)
+singleton x v = coerce (Map.singleton x v)
 
 unionSemigroup :: Semigroup a => Map a -> Map a -> Map a
 unionSemigroup (Map l) (Map r) = Map (Map.unionWith (<>) l r)
 
 foldrWithKey :: (CoVar -> a -> b -> b) -> b -> Map a -> b
-foldrWithKey f b (Map m) = Map.foldrWithKey (f . create) b m
-
-create :: Int -> CoVar
-create i = CoVar i (T.singleton '?') ValueVar
+foldrWithKey f b (Map m) = Map.foldrWithKey f b m
--- src/Data/VarSet.hs
+++ src/Data/VarSet.hs
@@ -10,8 +10,8 @@ module Data.VarSet
   ) where
 
 import qualified Data.HashSet as Set
-import qualified Data.Text as T
 import qualified Data.List as L
+
 import Prelude hiding (foldr, filter)
 
 import Core.Var
@@ -19,27 +19,27 @@ import Core.Var
 import Data.Coerce
 
 newtype Set
-  = Set (Set.HashSet Int)
+  = Set (Set.HashSet CoVar)
   deriving (Eq, Show, Ord)
   deriving newtype (Semigroup, Monoid)
 
 insert :: CoVar -> Set -> Set
-insert (CoVar x _ _) (Set k) = Set (Set.insert x k)
+insert x (Set k) = Set (Set.insert x k)
 
 fromList :: [CoVar] -> Set
 fromList = L.foldr insert mempty
 
 toList :: Set -> [CoVar]
-toList (Set s) = map fake (Set.toList s)
+toList (Set s) = Set.toList s
 
 foldr :: (CoVar -> a -> a) -> a -> Set -> a
-foldr f a (Set s) = Set.foldr (f . fake) a s
+foldr f a (Set s) = Set.foldr f a s
 
 member :: CoVar -> Set -> Bool
-member (CoVar x _ _) set = Set.member x (coerce set)
+member x set = Set.member x (coerce set)
 
 notMember :: CoVar -> Set -> Bool
-notMember (CoVar x _ _) set = not (Set.member x (coerce set))
+notMember x set = not (Set.member x (coerce set))
 
 difference :: Set -> Set -> Set
 difference = coerce Set.difference
@@ -51,16 +51,13 @@ intersection :: Set -> Set -> Set
 intersection = coerce Set.intersection
 
 singleton :: CoVar -> Set
-singleton (CoVar x _ _) = coerce (Set.singleton x)
+singleton x = coerce (Set.singleton x)
 
 delete :: CoVar -> Set -> Set
-delete (CoVar x _ _) set = coerce (Set.delete x (coerce set))
+delete x set = coerce (Set.delete x (coerce set))
 
 filter :: (CoVar -> Bool) -> Set -> Set
-filter f (Set s) = Set (Set.filter (f . fake) s)
+filter f (Set s) = Set (Set.filter f s)
 
 isEmpty :: Set -> Bool
 isEmpty = coerce Set.null
-
-fake :: Int -> CoVar
-fake x = CoVar x (T.singleton '?') ValueVar
