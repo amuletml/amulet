12b1dc371e44880005f035399484806819842321
Jonathan Coates <git@squiddev.cc>
2018-09-11 11:02:27 +0000

    Fix records not unifying or something
    I dunno, @zardyh told me how to do it.

---
+++ tests/types/pass_record_access.ml
@@ -0,0 +1,24 @@
+let access x = x.a + 0
+
+let access_2 x = x.a + x.b
+
+let access_match x =
+  (match x with | { a, b } -> a + b) + x.c
+
+let access_match_2 x =
+  (match x with | { a, b } -> a + b) + x.b
+
+let access_match_3 x =
+  (match x with | { a, b } -> a + b + x.c)
+
+let () =
+  let x = { a = 0, b = 0, c = 0 }
+
+  access x
+  access_2 x
+
+  access_match x
+  access_match_2 x
+  access_match_3 x
+
+  ()
