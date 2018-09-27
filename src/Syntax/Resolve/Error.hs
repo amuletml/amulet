ae44bbbff76cd954fcf30a661d7f843eed4dc2b5
SquidDev <bonzoweb@hotmail.co.uk>
2018-09-27 08:20:36 +0000

    Move resolve errors into a separate module
    They were beginning to get a little noisy

---
+++ src/Syntax/Resolve/Error.hs
@@ -0,0 +1,90 @@
+{-# LANGUAGE MultiParamTypeClasses, OverloadedStrings #-}
+module Syntax.Resolve.Error
+  ( ResolveError(..)
+  , VarKind(..)
+  ) where
+
+import Control.Applicative ((<|>))
+
+import qualified Data.Text as T
+import Data.Spanned
+import Data.Reason
+import Data.Maybe
+
+import Syntax.Pretty
+
+import Text.Pretty.Semantic
+import Text.Pretty.Note
+
+data VarKind
+  = VarVar
+  | VarCtor
+  | VarType
+  | VarTyvar
+  | VarModule
+  deriving (Show, Eq)
+
+-- | An error in the resolution process. Note that one error may be
+-- thrown multiple times.
+data ResolveError
+  = NotInScope VarKind (Var Parsed)   [Var Parsed] -- ^ This object was not in scope
+
+  | Ambiguous (Var Parsed) [Var Resolved] -- ^ This reference could refer to more than one variable
+  | NonLinearPattern (Var Resolved) [Pattern Resolved] -- ^ This pattern declares one variable multiple times
+  | NonLinearRecord (Expr Parsed) T.Text -- ^ This record declares an entry multiple times
+
+  | EmptyMatch -- ^ This @match@ has no patterns
+  | EmptyBegin -- ^ This @begin@ block has no expressions
+  | IllegalImplicit (Pattern Parsed) -- ^ When performing a pattern-matching let within an implicit bind.
+
+  -- | A wrapper for other errors which adds some additional context,
+  -- such as a source position.
+  | ArisingFrom ResolveError SomeReason
+  deriving (Show)
+
+instance Pretty VarKind where
+  pretty VarVar = "Variable"
+  pretty VarCtor = "Constructor"
+  pretty VarType = "Type"
+  pretty VarTyvar = "Type variable"
+  pretty VarModule = "Module"
+
+instance Pretty ResolveError where
+  pretty (NotInScope k e _) = pretty k <+> "not in scope:" <+> verbatim e
+
+  pretty (Ambiguous v _) = "Ambiguous reference to variable:" <+> verbatim v
+  pretty (NonLinearPattern v _) = "Non-linear pattern (multiple definitions of" <+> verbatim v <+> ")"
+  pretty (NonLinearRecord _ t) = "Duplicate field" <+> stypeSkol (text t) <+> "in record" <#> empty
+
+  pretty EmptyMatch = "Empty match expression"
+  pretty EmptyBegin = "Empty begin expression"
+  pretty (IllegalImplicit p) = "Invalid pattern" <+> verbatim p <+> "for implicit binding (should be a simple capture)"
+
+  pretty (ArisingFrom er ex) = pretty er <#> empty <#> nest 4 (string "Arising from use of" <+> blameOf ex </> pretty ex)
+
+instance Spanned ResolveError where
+  annotation (ArisingFrom _ x) = annotation x
+  annotation (NonLinearRecord e _) = annotation e
+  annotation (IllegalImplicit p) = annotation p
+  annotation _ = undefined
+
+instance Note ResolveError Style where
+  diagnosticKind _ = ErrorMessage
+
+  formatNote f (IllegalImplicit pat) =
+    vsep [ indent 2 $ string "Illegal implicit binding of" <+> (Right <$> highlight "pattern" <+> pretty pat)
+         , empty
+         , f [annotation pat]
+         , empty
+         , indent 2 . bullet $ string "Note: all implicit bindings must be of the form"
+         , empty
+         , indent 6 $ (Right <$> keyword "let implicit") <+> char 'f' <+> (Right <$> equals) <+> string "..."
+         , empty
+         , indent 4 $ string "where" <+> (Right <$> stypeVar (char 'f')) <+> string "is an identifier or function."
+         ]
+
+  formatNote f x = indent 2 (Right <$> pretty x) <#> fromJust (body x) where
+    body (ArisingFrom er a) = body er <|> Just (f [annotation a])
+    body (NonLinearPattern _ ps) = Just (f (map annotation ps))
+    body (NonLinearRecord e _) = Just (f [annotation e])
+    body _ = Nothing
