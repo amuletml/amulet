2e0e724fd62c74cc383caf9b1590d10139c21b07
SquidDev <bonzoweb@hotmail.co.uk>
2018-09-27 09:00:22 +0000

    Flesh out the constant folder a little
     - Add some rules for (in-)equality. Namely:
       - Literal integers, strings and booleans
       - Anything of type unit
       - Same variable for integers, strings, booleans and literals.
     - Add a test file to test the basic case of most of these
     - Fix a couple of bugs this exposed

--- src/Core/Optimise/Reduce/Fold.hs
+++ src/Core/Optimise/Reduce/Fold.hs
@@ -7,16 +7,19 @@ import qualified Data.Text as T
 
 import Core.Optimise.Reduce.Base
 import Core.Builtin
+import Core.Types
 
-foldApply :: CoVar -> [Atom a] -> Maybe (Term a)
+foldApply :: IsVar a => CoVar -> [Atom a] -> Maybe (Term a)
 foldApply v xs =
   case xs of
-    -- Primitive integer reductions
+    -- Complete integer reductions
     [Lit (Int l), Lit (Int r)] | v == vOpAdd -> num (l + r)
     [Lit (Int l), Lit (Int r)] | v == vOpSub -> num (l - r)
     [Lit (Int l), Lit (Int r)] | v == vOpMul -> num (l * r)
     [Lit (Int l), Lit (Int r)] | v == vOpDiv -> num (l `div` r)
     [Lit (Int l), Lit (Int r)] | v == vOpExp -> num (l ^ r)
+    [Lit (Int l), Lit (Int r)] | v == vOpEq -> bool (l == r)
+    [Lit (Int l), Lit (Int r)] | v == vOpNe -> bool (l /= r)
     [Lit (Int l), Lit (Int r)] | v == vOpLt -> bool (l < r)
     [Lit (Int l), Lit (Int r)] | v == vOpGt -> bool (l > r)
     [Lit (Int l), Lit (Int r)] | v == vOpLe -> bool (l <= r)
@@ -25,21 +28,44 @@ foldApply v xs =
     -- Partial integer reductions
     [x, Lit (Int 0)] | v == vOpAdd -> atom x
     [Lit (Int 0), x] | v == vOpAdd -> atom x
-    [Lit (Int 0), x] | v == vOpSub -> atom x
+    [x, Lit (Int 0)] | v == vOpSub -> atom x
     [x, Lit (Int 1)] | v == vOpMul -> atom x
     [Lit (Int 1), x] | v == vOpMul -> atom x
     [_, Lit (Int 0)] | v == vOpMul -> num 0
     [Lit (Int 0), _] | v == vOpMul -> num 0
     [x, Lit (Int 1)] | v == vOpDiv -> atom x
 
-    -- Primitive string reductions
+    -- Complete string reductions
     [Lit (Str l), Lit (Str r)]  | v == vOpConcat -> str (l `T.append` r)
+    [Lit (Str l), Lit (Str r)]  | v == vOpEq -> bool (l == r)
+    [Lit (Str l), Lit (Str r)]  | v == vOpNe -> bool (l /= r)
 
     -- Partial string reductions
     [x, Lit (Str "")] | v == vOpConcat -> atom x
     [Lit (Str ""), x] | v == vOpConcat -> atom x
 
+    -- Complete boolean reductions
+    [Lit l@LitTrue,  Lit r] | v == vOpEq -> bool (l == r)
+    [Lit l@LitFalse, Lit r] | v == vOpEq -> bool (l == r)
+    [Lit l@LitTrue,  Lit r] | v == vOpNe -> bool (l /= r)
+    [Lit l@LitFalse, Lit r] | v == vOpNe -> bool (l /= r)
+
+    -- Unit reductions (always equal, so no further checks needed)
+    [x, _] | v == vOpEq, approximateAtomType x == tyUnit -> bool True
+    [x, _] | v == vOpNe, approximateAtomType x == tyUnit -> bool False
+
+    -- Reduce (@@) operator
     [x, y] | v == vOpApp -> Just (App x y)
+
+    -- Equality of known values
+    [Ref l ty, Ref r _]
+      | v == vOpEq, l == r
+      , compareTy ty
+      -> bool True
+    [Ref l ty, Ref r _]
+      | v == vOpNe, l == r
+      , compareTy ty
+      -> bool False
 
     _ -> Nothing
   where
@@ -47,3 +73,8 @@ foldApply v xs =
     str = atom . Lit . Str
     bool x = atom (Lit (if x then LitTrue else LitFalse))
     atom = Just . Atom
+
+    compareTy t = t == tyBool
+               || t == tyInt
+               || t == tyString
+               || t == tyUnit
---
+++ tests/lua/opt_constant_fold.lua
@@ -0,0 +1,43 @@
+do
+  local __builtin_unit = { __tag = "__builtin_unit" }
+  local bottom = nil
+  bottom(function(je)
+    local i = je.i
+    local s = je.s
+    return {
+      int_add_c = 5,
+      int_sub_c = -1,
+      int_mul_c = 6,
+      int_div_c = 3,
+      int_exp_c = 8,
+      int_eq_c = false,
+      int_ne_c = true,
+      int_lt_c = true,
+      int_gt_c = false,
+      int_le_c = false,
+      int_ge_c = true,
+      int_add_l = i,
+      int_add_r = i,
+      int_sub_l = i,
+      int_mul1_l = i,
+      int_mul1_r = i,
+      int_mul_l = 0,
+      int_mul_r = 0,
+      int_div_l = i,
+      str_con_c = "foobar",
+      str_eq_c = false,
+      str_ne_c = true,
+      str_con_l = s,
+      str_con_r = s,
+      bol_eq_c = false,
+      bol_ne_c = true,
+      uni_eq = true,
+      uni_ne = false,
+      app = je.fn(je.x),
+      int_eq_u = true,
+      int_ne_u = false,
+      str_eq_u = true,
+      str_nq_u = false
+    }
+  end)
+end
