cdc6952ea273716c9240e8b1b86e775ac04d3271
SquidDev <bonzoweb@hotmail.co.uk>
2018-09-27 10:08:16 +0000

    Inline lazy values which are forced exactly once
    This is a very naive implementation, is sufficient for the basic
    implementation of (&&) and (||). A more complete implementation will
    come later, but will most likely require a separate pass.

--- src/Core/Optimise/Reduce/Fold.hs
+++ src/Core/Optimise/Reduce/Fold.hs
@@ -3,27 +3,23 @@ module Core.Optimise.Reduce.Fold
   ( foldApply
   ) where
 
+import Control.Lens
+
 import qualified Data.Text as T
 
 import Core.Optimise.Reduce.Base
 import Core.Builtin
 import Core.Types
 
+-- | Apply constant folding an application of some arbitrary function.
+--
+-- Note the function provided must be the "raw" variant (not applied to
+-- any types).
 foldApply :: IsVar a => CoVar -> [Atom a] -> Maybe (Term a)
+foldApply v _ | v ^. covarId >= 0 = Nothing -- Skip non-builtin functions
 foldApply v xs =
   case xs of
-    -- Complete integer reductions
-    [Lit (Int l), Lit (Int r)] | v == vOpAdd -> num (l + r)
-    [Lit (Int l), Lit (Int r)] | v == vOpSub -> num (l - r)
-    [Lit (Int l), Lit (Int r)] | v == vOpMul -> num (l * r)
-    [Lit (Int l), Lit (Int r)] | v == vOpDiv -> num (l `div` r)
-    [Lit (Int l), Lit (Int r)] | v == vOpExp -> num (l ^ r)
-    [Lit (Int l), Lit (Int r)] | v == vOpEq -> bool (l == r)
-    [Lit (Int l), Lit (Int r)] | v == vOpNe -> bool (l /= r)
-    [Lit (Int l), Lit (Int r)] | v == vOpLt -> bool (l < r)
-    [Lit (Int l), Lit (Int r)] | v == vOpGt -> bool (l > r)
-    [Lit (Int l), Lit (Int r)] | v == vOpLe -> bool (l <= r)
-    [Lit (Int l), Lit (Int r)] | v == vOpGe -> bool (l >= r)
+    [Lit l, Lit r] -> Atom . Lit <$> foldLiteral v [l, r]
 
     -- Partial integer reductions
     [x, Lit (Int 0)] | v == vOpAdd -> atom x
@@ -35,20 +31,9 @@ foldApply v xs =
     [Lit (Int 0), _] | v == vOpMul -> num 0
     [x, Lit (Int 1)] | v == vOpDiv -> atom x
 
-    -- Complete string reductions
-    [Lit (Str l), Lit (Str r)]  | v == vOpConcat -> str (l `T.append` r)
-    [Lit (Str l), Lit (Str r)]  | v == vOpEq -> bool (l == r)
-    [Lit (Str l), Lit (Str r)]  | v == vOpNe -> bool (l /= r)
-
     -- Partial string reductions
     [x, Lit (Str "")] | v == vOpConcat -> atom x
     [Lit (Str ""), x] | v == vOpConcat -> atom x
-
-    -- Complete boolean reductions
-    [Lit l@LitTrue,  Lit r] | v == vOpEq -> bool (l == r)
-    [Lit l@LitFalse, Lit r] | v == vOpEq -> bool (l == r)
-    [Lit l@LitTrue,  Lit r] | v == vOpNe -> bool (l /= r)
-    [Lit l@LitFalse, Lit r] | v == vOpNe -> bool (l /= r)
 
     -- Unit reductions (always equal, so no further checks needed)
     [x, _] | v == vOpEq, approximateAtomType x == tyUnit -> bool True
@@ -70,7 +55,6 @@ foldApply v xs =
     _ -> Nothing
   where
     num = atom . Lit . Int
-    str = atom . Lit . Str
     bool x = atom (Lit (if x then LitTrue else LitFalse))
     atom = Just . Atom
 
@@ -78,3 +62,42 @@ foldApply v xs =
                || t == tyInt
                || t == tyString
                || t == tyUnit
+
+-- | Attempt to fold an application to a builtin function with a series
+-- of literal values.
+foldLiteral :: CoVar -> [Literal] -> Maybe Literal
+foldLiteral v xs =
+  case xs of
+    -- Integer reductions
+    [Int l, Int r] | v == vOpAdd -> num (l + r)
+    [Int l, Int r] | v == vOpSub -> num (l - r)
+    [Int l, Int r] | v == vOpMul -> num (l * r)
+    [Int l, Int r] | v == vOpDiv -> num (l `div` r)
+    [Int l, Int r] | v == vOpExp -> num (l ^ r)
+    [Int l, Int r] | v == vOpEq -> bool (l == r)
+    [Int l, Int r] | v == vOpNe -> bool (l /= r)
+    [Int l, Int r] | v == vOpLt -> bool (l < r)
+    [Int l, Int r] | v == vOpGt -> bool (l > r)
+    [Int l, Int r] | v == vOpLe -> bool (l <= r)
+    [Int l, Int r] | v == vOpGe -> bool (l >= r)
+
+    -- String reductions
+    [Str l, Str r]  | v == vOpConcat -> str (l `T.append` r)
+    [Str l, Str r]  | v == vOpEq -> bool (l == r)
+    [Str l, Str r]  | v == vOpNe -> bool (l /= r)
+
+    -- Boolean reductions
+    [l@LitTrue,  r] | v == vOpEq -> bool (l == r)
+    [l@LitFalse, r] | v == vOpEq -> bool (l == r)
+    [l@LitTrue,  r] | v == vOpNe -> bool (l /= r)
+    [l@LitFalse, r] | v == vOpNe -> bool (l /= r)
+
+    -- Unit reductions
+    [Unit, _] | v == vOpEq -> bool True
+    [Unit, _] | v == vOpNe -> bool False
+
+    _ -> Nothing
+  where
+    num = Just . Int
+    str = Just . Str
+    bool x = Just (if x then LitTrue else LitFalse)
