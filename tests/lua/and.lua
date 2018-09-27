2a8f44ee3c980531ee386bc73943f1183f469cd8
Jonathan Coates <git@squiddev.cc>
2018-09-27 16:18:15 +0000

    Allow the backend to return expressions for a block of statements
    Previously 'emitTerm' would always return a sequence of statements. We
    now allow it to return an expression or series of statements. This
    allows us to specialise some terms where possible.
    
    We use this to generate 'and' and 'or' expressions, rather than a full
    if statement.

--- tests/lua/and.lua
+++ tests/lua/and.lua
@@ -1,9 +1,6 @@
 do
   local __builtin_unit = { __tag = "__builtin_unit" }
+  local function main(f) return f(1) and f(2) end
   local bottom = nil
-  if bottom(1) then
-    bottom(bottom(2))
-  else
-    bottom(false)
-  end
+  bottom(main)
 end
