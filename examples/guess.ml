1e0c10dfa1c7e67f6c5fb1c7399ad9c73efc665c
hydraz <urn@semi.works>
2018-10-18 01:22:35 +0800

    jesus that one was old

--- examples/guess.ml
+++ examples/guess.ml
@@ -1,45 +1,44 @@
-external val random : int -> int -> int = "math.random" ;;
-external val random_seed : int -> unit  = "math.randomseed" ;;
+external val random : int -> int -> int = "math.random"
+external val random_seed : int -> unit  = "math.randomseed"
 
-external val print_endline : string -> unit = "print" ;;
-external val print : string -> unit  = "io.write" ;;
-external val read : string -> string  = "io.read" ;;
+external val print_endline : string -> unit = "print"
+external val print : string -> unit  = "io.write"
+external val read : string -> string  = "io.read"
 
-external val current_time : unit -> int  = "os.time" ;;
+external val current_time : unit -> int  = "os.time"
 
-external val prim_int_of_string : string -> int  = "tonumber" ;;
-external val string_of_int : int -> string  = "tostring" ;;
+external val prim_int_of_string : string -> int  = "tonumber"
+external val string_of_int : int -> string  = "tostring"
 
-external val transmute : 'a -> 'b  = "(function(a) return a end)" ;;
+external val is_nil : 'a -> bool = "function(x) return x == nil end"
 
 type option 'a =
   | Just of 'a
-  | Nothing ;;
+  | Nothing
 
-let int_of_string str
-  = let vl = prim_int_of_string str in
-    if transmute vl == () then
-      Nothing
-    else
-      Just vl ;;
+let int_of_string str =
+  let vl = prim_int_of_string str
+  if is_nil vl then
+    Nothing
+  else
+    Just vl
 
 let read_line _ = read "*l"
-and main _ =
-  begin
-    random_seed (current_time ()) ;
-    let vl = random 1 10
-    and loop _ = begin
-      print "Guess a number between 1 and 10: " ;
-      let read = int_of_string (read_line ())
-       in match read with
-          | Just guess ->
-              if vl == guess then
-                print_endline "You got it right!"
-              else if vl > guess then begin
-                   print_endline "Too low!"; loop ()
-              end else if vl < guess then begin
-                   print_endline "Too high!"; loop ()
-              end else loop ()
-          | Nothing -> print_endline "Bye"
-    end in loop ()
-  end
+let main =
+  random_seed (current_time ())
+  let vl = random 1 10
+  let loop _ =
+    print "Guess a number between 1 and 10: "
+    match int_of_string (read_line ()) with
+    | Just guess ->
+        if vl == guess then
+          print_endline "You got it!"
+        else if vl > guess then
+          print_endline "Too low!"
+          loop ()
+        else if vl < guess then
+          print_endline "Too high!"
+          loop ()
+        else loop ()
+    | Nothing -> print_endline "Bye"
+  loop ()
