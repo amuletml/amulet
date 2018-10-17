5d0a3146c8ebbbb3d8e2e2823caa583c7cb62d53
Jonathan Coates <git@squiddev.cc>
2018-10-17 09:11:14 +0000

    Various improvements to the class/instance pre-parser
     - Handle empty class/instance blocks
     - Handle multi-line instance contexts/instances

--- tests/parser/pass_class.ml
+++ tests/parser/pass_class.ml
@@ -1,5 +1,18 @@
+class empty
+instance empty int
+
 class semigroup 'a
   val (<>) : 'a -> 'a -> 'a
 
 instance semigroup int
   let (<>) = (+)
+
+class multiline
+  'a
+  val foo : 'a
+  val bar : 'a
+
+instance multiline string
+  => multiline int
+  let foo = 1
+  let bar = 2
