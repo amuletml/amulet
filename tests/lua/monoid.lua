3a787d0ccb596a7db223c1dacf82bd1fcf3c5ec8
hydraz <urn@semi.works>
2019-03-14 16:14:13 -0300

    Invert instance desugaring
    Instead of making a single non-recursive dictionary binding that
    contains a binding group with all the methods ($dUse_old), we make a
    recursive binding group with the methods _and_ the dictionary
    ($use/$dUse_new).
    
    >  let $dUse_old =
    >    fun @a (cf : use br'a) -> let use : list br'a -> unit =
    >      fun f -> match f with
    >      | Nil -> ()
    >      | (Cons (_, xs)) ->
    >          use {list br'a} ($dUse_old @a cf) xs
    >    in Use @(list br'a) use
    >  let $use =
    >    fun @a (cg as cf) f -> match f with
    >    | Nil -> ()
    >    | (Cons (_, xs)) ->
    >        use {list br'a} ($dUse_new @a cg) xs
    >  and $dUse_new : {'a : type}. use 'a => use (list 'a) =
    >    fun @a (cg as cf : use br'a) -> Use @(list br'a) ($use @br'a cf)
    
    This should mean that the optimiser is able to inline methods and
    dictionaries, but it does result in generally better code anyway - See
    e.g. tests/lua/default-method, which is now entirely obsolete because
    the compiler sees through the lowered recursive dictionary and inlines
    the method.

--- tests/lua/monoid.lua
+++ tests/lua/monoid.lua
@@ -4,57 +4,46 @@ do
   local __builtin_unit = { __tag = "__builtin_unit" }
   local tostring = tostring
   local writeln = print
-  local function _dollardApplicativeafe(agw)
+  local function _dollardApplicativeafi(cdf)
     return {
-      ["<*>"] = function(cbi) return function(cbf) return agw["×"](cbi)(cbf) end end,
-      pure = function(cbp) return agw.zero end,
-      ["Applicative$kl"] = function(cau) return function(car) return car end end
+      pure = function(ccr) return cdf.zero end,
+      ["<*>"] = function(cci) return function(ccf) return cdf["×"](cci)(ccf) end end,
+      ["Applicative$kl"] = function(cbq) return function(cbn) return cbn end end
     }
   end
   local function _colon_colon(x) return function(y) return Cons({ _1 = x, _2 = y }) end end
-  local function _dollardShowarv(ath)
-    return function(cs)
-      if cs.__tag == "Nil" then
-        return "Nil"
-      elseif cs.__tag == "Cons" then
-        local cdn = cs[1]
-        return ath(cdn._1) .. " :: " .. _dollardShowarv(ath)(cdn._2)
-      end
+  local function _dollarshow(atm, cs)
+    if cs.__tag == "Nil" then
+      return "Nil"
+    elseif cs.__tag == "Cons" then
+      local cfc = cs[1]
+      return atm(cfc._1) .. " :: " .. _dollarshow(atm, cfc._2)
     end
   end
-  local function _dollardTraversablebhm(chi)
-    return function(chf)
-      return function(k)
-        return function(cu)
-          if cu.__tag == "Nil" then
-            return chf.pure(Nil)
-          elseif cu.__tag == "Cons" then
-            local chb = cu[1]
-            return chf["<*>"](chf["Applicative$kl"](_colon_colon)(k(chb._1)))(_dollardTraversablebhm(__builtin_unit)(chf)(k)(chb._2))
-          end
-        end
-      end
+  local function _dollartraverse(bip, cji, k, cu)
+    if cu.__tag == "Nil" then
+      return cji.pure(Nil)
+    elseif cu.__tag == "Cons" then
+      local cje = cu[1]
+      return cji["<*>"](cji["Applicative$kl"](_colon_colon)(k(cje._1)))(_dollartraverse(__builtin_unit, cji, k, cje._2))
     end
   end
-  local function _dollardMonoidbmc(chz)
-    return {
-      ["×"] = function(x)
-        return function(ys)
-          if x.__tag == "Nil" then
-            return ys
-          elseif x.__tag == "Cons" then
-            local cht = x[1]
-            return Cons({ _1 = cht._1, _2 = _dollardMonoidbmc(__builtin_unit)["×"](cht._2)(ys) })
-          end
-        end
-      end,
-      zero = Nil
-    }
+  local function _dollar_d7(bnn, x, ys)
+    if x.__tag == "Nil" then
+      return ys
+    elseif x.__tag == "Cons" then
+      local ckf = x[1]
+      return Cons({ _1 = ckf._1, _2 = _dollar_d7(__builtin_unit, ckf._2, ys) })
+    end
   end
-  local cjb = { _1 = 1, _2 = __builtin_unit }
-  writeln(_dollardShowarv(function(x)
+  local cll = { _1 = 1, _2 = __builtin_unit }
+  writeln(_dollarshow(function(x)
     return tostring(x)
-  end)(_dollardTraversablebhm(__builtin_unit)(_dollardApplicativeafe(_dollardMonoidbmc(__builtin_unit)))(function(ccg)
-    return Cons({ _2 = Nil, _1 = ccg._1 })
-  end)(Cons({ _1 = cjb, _2 = Cons({ _1 = cjb, _2 = Cons({ _1 = cjb, _2 = Nil }) }) }))))
+  end, _dollartraverse(__builtin_unit, _dollardApplicativeafi({
+    zero = Nil,
+    ["×"] = function(x) return function(ys) return _dollar_d7(__builtin_unit, x, ys) end end
+  }), function(cdq) return Cons({ _1 = cdq._1, _2 = Nil }) end, Cons({
+    _1 = cll,
+    _2 = Cons({ _1 = cll, _2 = Cons({ _1 = cll, _2 = Nil }) })
+  }))))
 end
--- tests/lua/recursive_defaults.lua
+++ tests/lua/recursive_defaults.lua
@@ -1,10 +1,10 @@
 do
   local __builtin_unit = { __tag = "__builtin_unit" }
   local use = print
-  local function _dollardShowci(fl)
+  local function _dollardShowci(fn)
     return {
-      show = function(fh) return "()" end,
-      ["show'"] = function(dy) return _dollardShowci(__builtin_unit).show(dy) end
+      show = function(ff) return "()" end,
+      ["show'"] = function(dz) return _dollardShowci(__builtin_unit).show(dz) end
     }
   end
   use(_dollardShowci(__builtin_unit).show)
