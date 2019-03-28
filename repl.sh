3a68f3b18b03bb00ab56d1ab5053cd934d459f74
Abigail Magalhães <magalhaes.alcantara@pucpr.edu.br>
2019-03-28 17:53:31 -0300

    Move to Stack
    Goodbye Nix, but I'm tired of your shit.

--- repl.sh
+++ repl.sh
@@ -2,21 +2,16 @@
 
 set -e
 
-if [ -z ${IN_NIX_SHELL+a} ]; then
-  echo -e "This program should be run as \x1b[1;31mnix-shell . --run '$0'\x1b[0m."
-  exit 1
-fi
-
-WARN="-Wextra -Wall -Wno-name-shadowing -Wno-implicit-prelude -Wno-missing-import-lists -Wredundant-constraints -Wmonomorphism-restriction"
+WARN="-Wextra -Wall -Wno-name-shadowing -Wno-implicit-prelude -Wno-missing-import-lists -Wredundant-constraints"
 
 # build lexers
 find src/ -type f -name '*.x' | while read -r fname; do
   source_mtime=$(stat -c %Y $fname)
   out_name="${fname%.x}.hs"
   if [[ ! -f $out_name ]]; then
-    alex $fname
+    stack exec -- alex $fname
   elif [[ $source_mtime > $(stat -c %Y $out_name) ]]; then
-    alex $fname
+    stack exec -- alex $fname
   else
     echo "already built lexer $fname"
   fi
@@ -27,9 +22,9 @@ find src/ -type f -name '*.y' | while read -r fname; do
   source_mtime=$(stat -c %Y $fname)
   out_name="${fname%.y}.hs"
   if [[ ! -f $out_name ]]; then
-    happy -g -c $fname
+    stack exec -- happy -g -c $fname
   elif [[ $source_mtime > $(stat -c %Y $out_name) ]]; then
-    happy -g -c $fname
+    stack exec -- happy -g -c $fname
   else
     echo "already built parser $fname"
   fi
@@ -39,7 +34,6 @@ if [[ $1 == "reload" ]]; then
   exit 0
 fi
 
-
 echo "Loading ghci.."
 
-exec ghci $WARN -i./src/:./compiler/ ./compiler/Main.hs $@
+exec stack exec -- ghci $WARN -i./src/:./compiler/ ./compiler/Main.hs $@
