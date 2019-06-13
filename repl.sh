885603f7c708805b55dda751daee0102561d6f28
SquidDev <bonzoweb@hotmail.co.uk>
2019-06-13 19:48:53 +0000

    Generate alex/happy files in a disjoint directory
    I just spent 5 minutes wondering why my changes weren't applying - turns
    out stack was using the pre-existing parser.

--- repl.sh
+++ repl.sh
@@ -4,14 +4,17 @@ set -e
 
 WARN="-Wextra -Wall -Wno-name-shadowing -Wno-implicit-prelude -Wno-missing-import-lists -Wredundant-constraints"
 
+out_dir=".stack-work/amulet"
+
 # build lexers
 find src/ -type f -name '*.x' | while read -r fname; do
   source_mtime=$(stat -c %Y $fname)
-  out_name="${fname%.x}.hs"
+  out_name="$out_dir/${fname%.x}.hs"
+  mkdir -p $(dirname $out_name)
   if [[ ! -f $out_name ]]; then
-    stack exec -- alex $fname
+    stack exec -- alex -o $out_name $fname
   elif [[ $source_mtime > $(stat -c %Y $out_name) ]]; then
-    stack exec -- alex $fname
+    stack exec -- alex -o $out_name $fname
   else
     echo "already built lexer $fname"
   fi
@@ -20,11 +23,12 @@ done
 # build parsers
 find src/ -type f -name '*.y' | while read -r fname; do
   source_mtime=$(stat -c %Y $fname)
-  out_name="${fname%.y}.hs"
+  out_name="$out_dir/${fname%.y}.hs"
+  mkdir -p $(dirname $out_name)
   if [[ ! -f $out_name ]]; then
-    stack exec -- happy -g -c $fname
+    stack exec -- happy -g -c -o $out_name $fname
   elif [[ $source_mtime > $(stat -c %Y $out_name) ]]; then
-    stack exec -- happy -g -c $fname
+    stack exec -- happy -g -c -o $out_name $fname
   else
     echo "already built parser $fname"
   fi
@@ -36,4 +40,4 @@ fi
 
 echo "Loading ghci.."
 
-exec stack exec -- ghci -O0 -j2 +RTS -A128M -RTS -fhide-source-paths $WARN -i./src/:./compiler/ ./compiler/Main.hs $@
+exec stack exec -- ghci -O0 -j2 +RTS -A128M -RTS -fhide-source-paths $WARN -i./src/:$out_dir/src/:./compiler/ ./compiler/Main.hs $@
