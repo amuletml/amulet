5730e35d80eb0090976060bcd9cf59a59280f2f0
SquidDev <bonzoweb@hotmail.co.uk>
2019-06-21 23:49:56 +0100

    Add dot operators
    Entirely nicked from OCaml, because I like bad ideas or something.
    
    Allows you to write infix operators for `x.[y]` as `let (.[]) x y`.

--- repl.sh
+++ repl.sh
@@ -24,11 +24,12 @@ done
 find src/ -type f -name '*.y' | while read -r fname; do
   source_mtime=$(stat -c %Y $fname)
   out_name="$out_dir/${fname%.y}.hs"
+  info_name="$out_dir/${fname%.y}.info"
   mkdir -p $(dirname $out_name)
   if [[ ! -f $out_name ]]; then
-    stack exec -- happy -g -c -o $out_name $fname
+    stack exec -- happy -g -c -o $out_name -i$info_name $fname
   elif [[ $source_mtime > $(stat -c %Y $out_name) ]]; then
-    stack exec -- happy -g -c -o $out_name $fname
+    stack exec -- happy -g -c -o $out_name -i$info_name $fname
   else
     echo "already built parser $fname"
   fi
