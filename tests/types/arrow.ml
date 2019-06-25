90356de83103d6e7cf89504802a5a735b4597a6e
hydraz <urn@semi.works>
2019-06-25 17:28:27 -0300

    Add value restriction

--- tests/types/arrow.ml
+++ tests/types/arrow.ml
@@ -6,4 +6,5 @@ instance functor ((->) 'r) begin
   let map f g = fun x -> f (g x)
 end
 
-let map_nested : forall 'f 'g 'a 'b. functor 'f * functor 'g => ('a -> 'b) -> 'f ('g 'a) -> 'f ('g 'b) = map map map
+let map_nested : forall 'f 'g 'a 'b. functor 'f * functor 'g => ('a -> 'b) -> 'f ('g 'a) -> 'f ('g 'b) =
+  fun x -> map map map x
