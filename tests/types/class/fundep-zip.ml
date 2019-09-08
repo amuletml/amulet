8a7fd163e27a6683adcd7a9c8714074cdbd850ef
hydraz <urn@semi.works>
2019-09-08 18:01:44 -0300

    Add a n-ary zip example using FDs
    Taken from «Understanding Functional Dependencies via
    Constraint Handling Rules»

---
+++ tests/types/class/fundep-zip.ml
@@ -0,0 +1,21 @@
+let x :: xs = Cons (x, xs)
+
+let zip2 xs ys =
+  match xs, ys with
+  | Cons (x, xs), Cons (y, ys) -> (x, y) :: zip2 xs ys
+  | _, _ -> []
+
+class zip 'a 'b 'c | 'c -> 'a, 'c -> 'b begin
+  val zip : list 'a -> list 'b -> 'c
+end
+
+instance zip 'a 'b (list ('a * 'b)) begin
+  let zip = zip2
+end
+
+instance zip ('a * 'b) 'c 'e => zip 'a 'b (list 'c -> 'e) begin
+  let zip xs ys zs = zip (zip2 xs ys) zs
+end
+
+let z3 (xs : list 'a) (ys : list 'b) (zs : list 'c) : list ('a * 'b * 'c) =
+  zip xs (zip ys zs)
