8a9726d0c8c84ee8c3b5a76777c2503699ed9d8b
Matheus Magalhães de Alcantara <urn@semi.works>
2019-10-12 19:16:41 -0300

    :thinking:

--- tests/lua/opt_unbox_args.ml
+++ tests/lua/opt_unbox_args.ml
@@ -1,6 +1,6 @@
 external val ( * ) : int -> int -> int = "function(x, y) return x * y end"
 external val ( - ) : int -> int -> int = "function(x, y) return x * y end"
-external val ( <= ) : int -> int -> bool = "function(x, y) return x > y end"
+external val ( <= ) : int -> int -> bool = "function(x, y) return x <= y end"
 
 let fib' x acc = if x <= 1 then acc
                  else fib' (x - 1) (acc * x)
