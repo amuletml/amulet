5ac6fb4534733bb0e9621e23fd1643ec488c02fc
Jonathan Coates <git@squiddev.cc>
2019-10-18 14:33:14 +0100

    Start lowering and linting all TC tests
    While this may seem a little overboard, type-checking tests (obviously)
    have the largest coverage of the type system, and so it makes sense to
    check all these features can be successfully lowered.
    
    Note, this creates a lower test for /every/ TC test, irrespective of
    whether it actually TCs or not. This is unideal, but the easiest thing
    to do for now.

--- tests/driver/Test/Types/Check.hs
+++ tests/driver/Test/Types/Check.hs
@@ -1,12 +1,15 @@
 module Test.Types.Check (tests) where
 
+import Test.Tasty.HUnit
 import Test.Util
 
 import Control.Monad.Infer (names, firstName)
+import Control.Monad.IO.Class
 import Control.Monad.Namey
 import Control.Lens ((^.), to, runIdentity)
 
 import qualified Data.Text.Lazy as L
+import qualified Data.Text.IO as T
 import qualified Data.Text as T
 import qualified Data.Map as Map
 
@@ -20,8 +23,14 @@ import Syntax.Desugar (desugarProgram)
 import Syntax.Types (difference, toMap)
 import Syntax.Builtin
 
+import Core.Lower
+import Core.Lint
+
+import System.Directory
+import System.FilePath
+
 import qualified Text.Pretty.Note as N
-import Text.Pretty.Semantic
+import Text.Pretty.Semantic hiding ((</>))
 
 result :: String -> T.Text -> T.Text
 result f c = runIdentity . flip evalNameyT firstName $ do
@@ -45,16 +54,40 @@ result f c = runIdentity . flip evalNameyT firstName $ do
 
     prettyErrs = vsep . map (N.format (N.fileSpans [(f, c)] N.defaultHighlight))
 
+checkLint :: String -> Assertion
+checkLint file = flip evalNameyT firstName $ do
+  c <- liftIO (T.readFile file)
+  let parsed = requireJust file c $ runParser file (L.fromStrict c) parseTops
+  ResolveResult resolved _ _ <- requireRight file c <$> runNullImport (resolveProgram builtinResolve parsed)
+
+  desugared <- desugarProgram resolved
+  inferred <- toEither <$> inferProgram builtinEnv desugared
+  case inferred of
+    Left _ -> pure ()
+    Right (prg, _) -> do
+      lowered <- runLowerT (lowerProg prg)
+      case runLintOK (checkStmt emptyScope lowered) of
+        Nothing -> pure ()
+        Just (_, es) -> liftIO $ assertFailure ("Core lint failed:\n" ++ T.unpack (displayDetailed (pretty es)))
+
 tests :: IO TestTree
 tests = do
-  inference <- testGroup "Type inference tests" <$> goldenDir result "tests/types/" ".ml"
-  gadts <- testGroup "GADT inference tests" <$> goldenDir result "tests/types/gadt/" ".ml"
-  rankn <- testGroup "Rank-N inference tests" <$> goldenDir result "tests/types/rankn/" ".ml"
-  lazy <- testGroup "Automatic laziness tests" <$> goldenDir result "tests/types/lazy/" ".ml"
-  letgen <- testGroup "Let generalization tests" <$> goldenDir result "tests/types/letgen/" ".ml"
-  wild <- testGroup "Type wildcard tests" <$> goldenDir result "tests/types/wildcards/" ".ml"
-  clss <- testGroup "Type class tests" <$> goldenDir result "tests/types/class/" ".ml"
-  vta <- testGroup "Visible type application tests" <$> goldenDir result "tests/types/vta/" ".ml"
-  syn <- testGroup "Type synonym tests " <$> goldenDir result "tests/types/synonym/" ".ml"
-  tyfun <- testGroup "Type function tests " <$> goldenDir result "tests/types/tyfun/" ".ml"
-  pure (testGroup "Type inference" [ inference, gadts, rankn, lazy, letgen, wild, clss, vta, syn, tyfun ])
+  golden <- traverse (\(group, dir) -> testGroup group <$> goldenDir result dir ".ml") groups
+  lint <- traverse makeLint groups
+  pure $ testGroup "Types" [ testGroup "Inference" golden, testGroup "Lint" lint ]
+  where
+    makeLint (group, dir) = do
+      files <- filter (isExtensionOf "ml") <$> listDirectory dir
+      pure $ testGroup group (map (\x -> testCase x (checkLint (dir </> x))) files)
+    groups =
+      [ ("Default tests", "tests/types/")
+      , ("GADT tests", "tests/types/gadt/")
+      , ("Rank-N tests", "tests/types/rankn/")
+      , ("Automatic laziness tests", "tests/types/lazy/")
+      , ("Let generalization tests", "tests/types/letgen/")
+      , ("Type wildcard tests", "tests/types/wildcards/")
+      , ("Type class tests", "tests/types/class/")
+      , ("Visible type application tests", "tests/types/vta/")
+      , ("Type synonym tests ", "tests/types/synonym/")
+      , ("Type function tests ", "tests/types/tyfun/")
+      ]
