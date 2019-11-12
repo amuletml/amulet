1d6d4631208a3b5fbc671b6adc1bd666015bc9e1
Matheus Magalhães de Alcantara <matheus.de.alcantara@gmail.com>
2019-11-12 19:58:02 -0300

    Add a script to hlint changed files

---
+++ tools/lint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Check hlint style only on changed files. Useful as a pre-commit hook:
+#   ln -s $PWD/tools/lint.sh .git/hooks/pre-commit
+
+set -e
+
+check() {
+  git status --porcelain | while read -r file; do
+    if echo $file | grep -E '(M|\?\?|A)' | grep -E '.hs$' &>/dev/null; then
+      hlint $(echo $file | cut -d' ' -f2) || true
+    fi
+  done
+}
+
+if check | grep '.'; then
+  exit 1
+else
+  exit 0
+fi
