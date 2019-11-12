172cfa35da537fc336142bac7c560987ba70566f
Matheus Magalhães de Alcantara <matheus.de.alcantara@gmail.com>
2019-11-12 20:33:52 -0300

    generate sig file for pacman

--- tools/sign.sh
+++ tools/sign.sh
@@ -12,4 +12,9 @@ ls result/* | while read -r file; do
     gpg --batch --passphrase-fd 0 --detach-sign \
         --pinentry-mode loopback \
         --armor $file
+
+  echo $SIGNING_KEY_PW | \
+    gpg --batch --passphrase-fd 0 --detach-sign \
+        --pinentry-mode loopback \
+        $file
 done
