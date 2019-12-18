e5b2aeab68bcc1b4b2b5232a46f38a2e189e649f
Abigail Magalhães <magalhaes.alcantara@pucpr.edu.br>
2019-12-18 19:47:02 -0300

    Fix emitting top-level code in the Lua backend

--- src/Backend/Lua/Postprocess.hs
+++ src/Backend/Lua/Postprocess.hs
@@ -66,6 +66,7 @@ addBuiltins stmt state = fmap (flip (set topExVars) state) . swap
     opsStmt LuaBreak = mempty
     opsStmt (LuaCallS f) = opsCall f
     opsStmt LuaQuoteS{} = mempty
+    opsStmt LuaRawS{} = mempty
 
     opsExpr :: LuaExpr -> VarSet.Set
     opsExpr LuaNil = mempty
