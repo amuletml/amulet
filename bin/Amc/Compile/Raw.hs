41334fa71756ae725d10ef36bc24b012799fd526
Jonathan Coates <git@squiddev.cc>
2020-01-02 19:08:59 +0000

    Move the C shim into a separate file

--- bin/Amc/Compile/Raw.hs
+++
@@ -1,8 +0,0 @@
-module Amc.Compile.Raw (raw) where
-
-import Language.Haskell.TH.Quote
-import Language.Haskell.TH
-
-raw :: QuasiQuoter
-raw = QuasiQuoter { quoteExp = stringE, quotePat = undefined, quoteType = undefined, quoteDec = undefined }
-
