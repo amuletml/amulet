826a203d2fc1bb8029421974184de23a8c79e46c
Jonathan Coates <git@squiddev.cc>
2020-04-05 16:28:39 +0100

    Add a hashable instance for lists
    Strictly speaking this could be generalised to any foldable, but eh.

--- lib/data/hash.ml
+++ lib/data/hash.ml
@@ -26,6 +26,14 @@ instance hashable string begin
     let open import "../lua/string.ml"
     let rec go x = function
     | 0 -> x
-    | i -> go (x * 33 + char_code_at xs i) (i - 1)
+    | i -> go ((x * 33 + char_code_at xs i) .&. 0xFFFFFFFF) (i - 1)
     go 5381 (length xs)
+end
+
+instance hashable 'a => hashable (list 'a) begin
+  let hash xs =
+    let rec go h = function
+    | Nil -> h
+    | Cons (x, xs) -> go ((h * 31 + hash x) .&. 0xFFFFFFFF) xs
+    go 0 xs
 end
