bb4de4d832a7faf6833308eeafe0bd9902c1051b
Jonathan Coates <git@squiddev.cc>
2021-08-30 11:25:13 +0100

    Catch process errors when compiling Amulet
    Fixes #298. I think at least!

--- bin/Version.hs
+++ bin/Version.hs
@@ -5,11 +5,21 @@ import Language.Haskell.TH.Syntax
 import Language.Haskell.TH
 
 import System.Process
+import System.Exit
 
 gitRevision :: ExpQ
 gitRevision = do
   addDependentFile ".git/logs/HEAD"
-  [| $(stringE =<< runIO (init <$> readProcess "git" ["rev-parse", "--short", "@"] "")) :: String  |]
+  [| $(stringE =<< runIO (init <$> version)) :: String  |]
+
+  where
+    version :: IO String
+    version = do
+      (exit, out, _) <- readProcessWithExitCode "git" ["rev-parse", "--short", "@"] ""
+      pure $ case exit of
+        ExitSuccess -> init out
+        ExitFailure _ -> "??"
+
 
 versionStr :: String
 #if defined(VERSION_amuletml)
